4706401681e8c318f1a4455c3e279987dc1628cd
lobnaelnahas10 <90216856+lobnaelnahas10@users.noreply.github.com>
2021-11-28 23:58:50 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, {useEffect , Component} from 'react';
-import { Container,AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
-import AllFlights from './components/AdminComponents/allFlights';
-import useStyles from './styles';
-import { useDispatch } from 'react-redux';
-import CreateFlight from './components/AdminComponents/createFlight';
-import Navbar from './components/AdminComponents/Navbar/navbar'
-import Edit from './components/AdminComponents/edit';
-import SearchFlights from './components/AdminComponents/Search/searchFlights';
-import SearchResults from './components/AdminComponents/Search/searchResults';
+import logo from './logo.svg';
+import './App.css';
 
-
-
-const App = () => {
-    return(
-    <Router>
-            <div>
-                <Navbar />
-                <Route path="/api/flight" exact component={AllFlights} />
-                <Route path="/api/flight/create"exact component={CreateFlight} />
-                <Route path="/api/flight/updateFlightbyadmin/:id" exact component={Edit} />
-                <Route path = "/api/flight/searchFlight" exact component = {SearchFlights}/> 
-                <Route path="/list-search/:id" exact component={SearchResults}/>
-            </div>
-        </Router>
-     
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
